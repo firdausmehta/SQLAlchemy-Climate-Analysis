cc5b4e7d4ebc278ba2811673117b1c6dcaacf3a0
Firdaus Mehta <firmehta97@gmail.com>
2021-07-16 11:07:39 +1000

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,22 @@ latestDay = int(dt.datetime.strftime(latestDate, '%d'))
 
 yearBefore = dt.date(latestYear, latestMonth, latestDay) - dt.timedelta(days=365)
 yearBefore = dt.datetime.strftime(yearBefore, '%Y-%m-%d')
+
+
+
+
+@app.route("/")
+def home():
+    return (f"Welcome to Surf's Up!: Hawai'i Climate API<br/>"
+            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>"
+            f"Available Routes:<br/>"
+            f"/api/v1.0/stations ~~~~~ a list of all weather observation stations<br/>"
+            f"/api/v1.0/precipitaton ~~ the latest year of preceipitation data<br/>"
+            f"/api/v1.0/temperature ~~ the latest year of temperature data<br/>"
+            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>"
+            f"~~~ datesearch (yyyy-mm-dd)<br/>"
+            f"/api/v1.0/datesearch/2015-05-30  ~~~~~~~~~~~ low, high, and average temp for date given and each date after<br/>"
+            f"/api/v1.0/datesearch/2015-05-30/2016-01-30 ~~ low, high, and average temp for date given and each date up to and including end date<br/>"
+            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~<br/>"
+            f"~ data available from 2010-01-01 to 2017-08-23 ~<br/>"
+            f"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
